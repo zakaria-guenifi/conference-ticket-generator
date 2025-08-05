dd27b4046cb63078f1dea0a850930ae629306af8
zakaria-guenifi <Zakariaguenifi48@gmail.com>
2025-08-05 10:08:33 +0100

    Add files via upload

--- README.md
+++ README.md
@@ -1,1 +1,61 @@
-# conference-ticket-generator
+# Frontend Mentor - Conference ticket generator solution
+
+This is a solution to the [Conference ticket generator challenge on Frontend Mentor](https://www.frontendmentor.io/challenges/conference-ticket-generator-oq5gFIU12w).
+
+## Table of contents
+- [Overview](#overview)
+  - [The challenge](#the-challenge)
+  - [Screenshot](#screenshot)
+  - [Links](#links)
+- [My process](#my-process)
+  - [Built with](#built-with)
+  - [What I learned](#what-i-learned)
+- [Author](#author)
+
+## Overview
+This project was a bit more interesting than the previous Frontend Mentor challenges I did. It felt complex at first, but once I started building, things came together pretty smoothly and it ended up being a fun experience overall.
+
+### The challenge
+Users should be able to:
+
+- Complete the form with their details
+- Receive form validation messages if:
+  - Any field is missed
+  - The email address is not formatted correctly
+  - The avatar upload is too big or the wrong image format
+- Complete the form only using their keyboard
+- Have inputs, form field hints, and error messages announced on their screen reader
+- See the generated conference ticket when they successfully submit the form
+- View the optimal layout for the interface depending on their device's screen size
+- See hover and focus states for all interactive elements on the page
+
+### Screenshot
+
+![](./screenshot-desktop-form.jpg)![](./screenshot-desktop-ticket.jpg)![](./screenshot-mobile-form.jpg)![](./screenshot-mobile-ticket.jpg)
+
+### Links
+
+- Solution URL: [github repo](https://github.com/zakaria-guenifi/conference-ticket-generator)
+- Live Site URL: [pages live url](https://zakaria-guenifi.github.io/conference-ticket-generator/)
+
+## My process
+Since I didn’t have the Figma file for this challenge, I tried something new: I brought the design images into Figma and manually measured the font sizes, spacing, and element dimensions. I then used those measurements to build the CSS design system and utility classes.
+
+The HTML structure was fairly simple to set up. After that, I built the components one by one. Finally, I handled responsiveness, going back and forth as needed. I used clamp() for font sizes and letter spacing, and only used media queries for the body backgrounds and ticket sizing.
+
+### Built with
+
+- Semantic HTML5 markup
+- CSS custom properties
+- Flexbox
+- CSS Grid
+- Mobile-first workflow
+- Vanilla JS
+
+### What I learned
+Setting up the code to handle the form and the file input was new to me, I learned a lot about file input and forms and the way they should be handled and validated.
+
+## Author
+
+- Website - [guenifi.com](https://www.guenifi.com)
+- Frontend Mentor - [@zakaria-guenifi](https://www.frontendmentor.io/profile/zakaria-guenifi)
---
+++ script.js
@@ -0,0 +1,119 @@
+const uploadSection = document.querySelector(".upload-section");
+const formSection = document.querySelector(".form-section");
+const customInputField = document.querySelector(".custom-file-input");
+const inputFile = document.getElementById("file-input");
+const infoIcon = document.getElementById("info-icon");
+const uploadFeedback = document.getElementById("upload-feedback");
+const uploadImg = document.getElementById("upload-img");
+const uploadInstruction = document.getElementById("upload-instruction");
+const imgBtns = document.querySelector(".img-buttons");
+const removeImg = document.getElementById("remove-img");
+const changeImg = document.getElementById("change-img");
+const fullName = document.getElementById("full-name");
+const emailAddress = document.getElementById("email-address");
+const emailInfo = document.getElementById("email-info");
+const githubUsername = document.getElementById("github-username");
+const submitBtn = document.getElementById("submit-button");
+const ticketSection = document.querySelector(".ticket-generated");
+const emailSpan = document.getElementById("email-span");
+const nameOneSpan = document.getElementById("name-1-span");
+const nameTwoSpan = document.getElementById("name-2-span");
+const ticketDate = document.getElementById("ticket-date");
+const ticketName = document.getElementById("ticket-name");
+const ticketGithub = document.getElementById("ticket-github");
+const ticketAvatar = document.getElementById("ticket-avatar");
+const ticketCode = document.getElementById("ticket-code");
+
+
+// Upload Avatar logic
+inputFile.addEventListener("change", ()=> {
+    const fileSize = (inputFile.files[0].size / 1024);
+    
+    if ( fileSize > 500) {
+        uploadFeedback.textContent = "File too large. Please upload a photo under 500KB";
+        uploadFeedback.classList.add("error-message");
+        infoIcon.classList.add("error-message");
+        
+        uploadImg.src = "./assets/images/icon-upload.svg";
+        uploadImg.style.padding = "var(--sp-300)";
+        inputFile.value = "";
+
+    } else {
+        uploadFeedback.textContent = "Upload your photo (JPG or PNG, max size: 500KB).";
+        uploadFeedback.classList.remove("error-message");
+        infoIcon.classList.remove("error-message");
+
+        uploadImg.src = URL.createObjectURL(inputFile.files[0]);
+        uploadImg.style.padding = "0";
+
+        uploadInstruction.style.display = "none";
+
+        imgBtns.style.display = "flex";
+    }
+    
+});
+
+customInputField.addEventListener("keydown", (e) => {
+    if (e.key === "Enter" || e.key === " ") {
+        e.preventDefault();
+        inputFile.click();
+    }
+});
+
+removeImg.addEventListener("click", ()=> {
+    inputFile.value = "";
+
+    uploadInstruction.style.display = "block";
+    imgBtns.style.display = "none";
+
+    uploadImg.src = "./assets/images/icon-upload.svg";
+    uploadImg.style.padding = "var(--sp-300)";
+});
+
+changeImg.addEventListener("click", ()=> {
+    inputFile.click();
+});
+
+// Form validation logic
+emailAddress.addEventListener("input", ()=> {
+    const emailValid = /^[^\s@]+@[^\s@]+\.[^\s@]+$/.test(emailAddress.value.trim());
+    if (!emailValid) {
+        emailInfo.style.display = "flex";
+        emailAddress.style.marginBlockEnd = "var(--sp-400)";
+    } else {
+        emailInfo.style.display = "none";
+        emailAddress.style.marginBlockEnd = "";
+
+    }
+});
+
+
+
+
+
+// Generated ticket logic
+submitBtn.addEventListener("click", (e)=> {
+    
+    if ( 
+        fullName.value.trim() === "" 
+        || emailAddress.value.trim() === "" 
+        || githubUsername.value.trim() === "" ) {
+        e.preventDefault();
+    } else {
+        uploadSection.style.display = "none";
+        formSection.style.display = "none";
+        ticketSection.style.display = "block";
+        
+        emailSpan.textContent = emailAddress.value.trim();
+        nameOneSpan.textContent = `${fullName.value.trim().split(" ")[0]} `;
+        nameTwoSpan.textContent = fullName.value.trim().split(" ")[1];
+        
+
+        ticketDate.textContent = new Date(Date.now() + 30 * 24 * 60 * 60 * 1000).toDateString().slice(4);
+        ticketName.textContent = fullName.value.trim();
+        ticketGithub.textContent = githubUsername.value.trim();
+        ticketAvatar.src = URL.createObjectURL(inputFile.files[0]);
+        ticketCode.textContent = Math.floor(10000 + Math.random() * 90000);
+
+    };
+});
